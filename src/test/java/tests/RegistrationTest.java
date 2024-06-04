40f93a3a9b216b2001728090f6a2fea76f5760cc
Yana Avvakumova <10.07.93t@gmail.com>
2024-06-04 13:44:15 +0300

    HW 11 - version 2

--- src/test/java/tests/RegistrationTest.java
+++ src/test/java/tests/RegistrationTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 import static io.qameta.allure.Allure.step;
@@ -10,6 +11,7 @@ public class RegistrationTest extends TestBase {
 
     private final RegistrationPage registrationPage = new RegistrationPage();
 
+    @Tag("main")
     @DisplayName("Проверка заполнения и отправки всех полей формы")
     @Test
     void allFormFieldsTest() {
